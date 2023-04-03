836b1d8bedafbd46acc4a020c4c740d95ef2ad0e
igorcrexito <igorcrexito@gmail.com>
2023-04-02 23:30:31 -0300

    criando funcao de plot e arquivo de configuracao

--- main.py
+++ main.py
@@ -3,37 +3,59 @@ from model_architectures.autoencoder import Autoencoder
 from PIL import Image
 import numpy as np
 import random
+import yaml
 
 dataset_base_path = '../spoofing_dataset/training_real/'
-augmentation_list = ["flip", "zoom", "rotate"]
-input_modalities = ['rgb']
-model_list = []
 
-## training parameters
-number_of_epochs = 10
-batch_size = 50
-image_size = 112
+
+def plot_reconstructed_images(model_list: list, database: list, image_size: int, model_index: int):
+    """
+    This is just an auxiliary method that is going to be deleted in the future
+    """
+    number_of_images_to_be_reconstructed = 10
+    for i in range(number_of_images_to_be_reconstructed):
+        sample = random.randint(0, len(database) - 1)
+        image = database[sample].reshape(1, image_size, image_size, 3)
+        reconstructed_image = model_list[model_index].model.predict(image)
+        reconstructed_image = Image.fromarray(np.uint8(reconstructed_image[0] * 255))
+
+        reconstructed_image.show()
+
 
 if __name__ == '__main__':
 
+    print("Reading the configuration yaml the stores the executation variables")
+    with open("execution_parameters.yaml", "r") as f:
+        params = yaml.full_load(f)
+
     print("Creating a database based on the specified base path")
-    image_dataset = ImageDataset(base_path=dataset_base_path, image_size=(image_size, image_size), augmentation_list=augmentation_list)
+    image_dataset = ImageDataset(base_path=dataset_base_path,
+                                 image_size=(params["application_parameters"]["image_size"],
+                                             params["application_parameters"]["image_size"]),
+                                 augmentation_list=params["input_parameters"]["augmentation_list"])
 
     print("Loading the images and applying the data augmentation operations")
     database = np.array(image_dataset.load_data(), dtype='float16')
 
     print("Creating a simple autoencoder model for each input modality")
-    for modality in input_modalities:
-        model_list.append(Autoencoder(summarize_model=True, input_dimension=image_size, input_modality=modality))
-        model_list[0].fit_model(input_data=database, validation_data=database, number_of_epochs=number_of_epochs, batch_size=batch_size)
+    model_list = []
+    for index, modality in enumerate(params["input_parameters"]["input_modalities"]):
+        model_list.append(Autoencoder(summarize_model=False,
+                                      input_dimension=params["application_parameters"]["image_size"],
+                                      input_modality=modality))
 
+        if params["application_parameters"]["application_mode"] == "training":
+            print(f"Training the model for the modality {modality}")
+            model_list[index].fit_model(input_data=database, validation_data=database,
+                                        number_of_epochs=params["model_parameters"]["number_of_epochs"],
+                                        batch_size=params["model_parameters"]["batch_size"])
+        else:
+            print("Loading previously trained models")
+            model_list[0].model.load_weights(f"trained_models/best_autoencoder_{modality}.hdf5")
 
-    ## just testing the reconstruction for some images
-    #number_of_images_to_be_reconstructed = 10
-    #for i in range(number_of_images_to_be_reconstructed):
-    #    sample = random.randint(0, len(database)-1)
-    #    image = database[sample].reshape(1, image_size, image_size, 3)
-    #    reconstructed_image = model_list[0].model.predict(image)
-    #    reconstructed_image = Image.fromarray(np.uint8(reconstructed_image[0]*255))
-
-    #    reconstructed_image.show()
+    if params["application_parameters"]["plot_reconstruction"]:
+        print("Plotting some images to check the reconstruction")
+        plot_reconstructed_images(model_list=model_list,
+                                  database=database,
+                                  image_size=params["application_parameters"]["image_size"],
+                                  model_index=0)
